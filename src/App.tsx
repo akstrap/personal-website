36af7c785a6ffd6517f15e0b304e3c441d1f435e
Akshit Thavanati <akshitthavanati@Mac.lan>
2025-03-13 11:37:14 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center mt-10">
+      Hello, Portfolio!
+    </div>
+  );
+}
+
+export default App;
